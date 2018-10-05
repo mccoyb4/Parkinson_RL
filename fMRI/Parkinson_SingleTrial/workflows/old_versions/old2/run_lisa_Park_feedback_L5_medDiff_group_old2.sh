8847abb6ad583383ada33b0e638f28476bb73600
Bronagh McCoy <mccoyb4@users.noreply.github.com>
2018-10-05 10:19:37 +0200

    Delete run_lisa_Park_feedback_L5_medDiff_group_old2.sh

--- fMRI/Parkinson_SingleTrial/workflows/old_versions/old2/run_lisa_Park_feedback_L5_medDiff_group_old2.sh
+++
@@ -1,7 +0,0 @@
-#PBS -lwalltime=8:00:00 -lnodes=1:mem64gb
-
-module load eb Python/2.7.14-foss-2017b
-module load eb fsl/5.08
-PROJ_PATH=/nfs/bromccoy
-
-python /home/bromccoy/bash_scripts/Park_pipeline/Park/Parkinson_SingleTrial/workflows/old_versions/old2/Park_feedback_L5_medDiff_group_old2.py '0' 'lisa'
